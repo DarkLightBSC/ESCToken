27b94aaa0ff4237fbb534c88a7e52e14f9dd2194
ESCToken <85036926+minus07dd@users.noreply.github.com>
2021-08-28 00:49:47 +0800

    Delete 阿比斯 directory

--- 阿比斯/Route02.ts
+++
@@ -1,8 +0,0 @@
-const Route02_ABI = [
-    'function swapExactETHForTokens(uint256 amountOutMin, address[] calldata path, address to, uint256 deadline) payable returns (uint256[] memory amounts)',
-    'function swapTokensForExactTokens(uint amountOut,uint amountInMax,address[] calldata path,address to,uint deadline) external returns (uint[] memory amounts)',
-    'function addLiquidityETH(address token,uint amountTokenDesired,uint amountTokenMin,uint amountETHMin,address to,uint deadline) external payable returns (uint amountToken, uint amountETH, uint liquidity)',
-    'function addLiquidity(address tokenA,address tokenB,uint amountADesired,uint amountBDesired,uint amountAMin,uint amountBMin,address to,uint deadline) external returns (uint amountA, uint amountB, uint liquidity)'
-];
-
-export {Route02_ABI};
